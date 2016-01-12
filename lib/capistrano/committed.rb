667f857b3ecc909da0dac62ec23a4e0d685ee24b
Sam Bauers <sam@redant.com.au>
2016-01-13 09:44:16 +1100

    Reduce cyclomatic complexity for CodeClimate

--- lib/capistrano/committed.rb
+++ lib/capistrano/committed.rb
@@ -6,33 +6,19 @@ module Capistrano
   module Committed
     class << self
       def get_issue_urls(issue_pattern, postprocess, url_pattern, message)
-        fail TypeError, t('committed.error.helpers.valid_param',
-                          method: __callee__,
-                          param: 'issue_pattern') unless
-                            issue_pattern.is_a?(String) ||
-                            issue_pattern.is_a?(Regexp)
+        check_type __callee__,
+                   'issue_pattern',
+                   (issue_pattern.is_a?(String) || issue_pattern.is_a?(Regexp))
 
-        fail TypeError, t('committed.error.helpers.valid_param',
-                          method: __callee__,
-                          param: 'postprocess') unless
-                            postprocess.is_a?(Array)
-
+        check_type __callee__, 'postprocess', postprocess.is_a?(Array)
         postprocess.each { |method|
-          fail TypeError, t('committed.error.helpers.valid_param',
-                          method: __callee__,
-                          param: format('postprocess[:%s]', method.to_s)) unless
-                            method.is_a?(Symbol)
+          check_type __callee__,
+                     format('postprocess[:%s]', method.to_s),
+                     method.is_a?(Symbol)
         }
 
-        fail TypeError, t('committed.error.helpers.valid_param',
-                          method: __callee__,
-                          param: 'url_pattern') unless
-                            url_pattern.is_a?(String)
-
-        fail TypeError, t('committed.error.helpers.valid_param',
-                          method: __callee__,
-                          param: 'message') unless
-                            message.is_a?(String)
+        check_type __callee__, 'url_pattern', url_pattern.is_a?(String)
+        check_type __callee__, 'message', message.is_a?(String)
 
         matches = message.scan(Regexp.new(issue_pattern))
         return [] unless matches
@@ -53,6 +39,13 @@ module Capistrano
           output << format('%s   - %s', pad, url)
         end
         output << format('%s', pad)
+      end
+
+    private
+      def check_type(method, param, condition)
+        fail TypeError, t('committed.error.helpers.valid_param',
+                        method: method,
+                        param: param) unless condition
       end
     end
   end
